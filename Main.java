3a7dd13c8a5a975ca1e9dc25dbde7d52a16fbdcc
KochnevOleg <95541637+KochnevOleg@users.noreply.github.com>
2022-04-04 18:52:37 +0300

    initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+package Unit2.JavaCore.Calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = Calculator.instance.get();
+
+        int x = calculator.plus.apply(3,67);
+        int y = calculator.divide.apply(190, 54);
+        int z = calculator.pow.apply(4);
+        int a = calculator.minus.apply(45,78);
+        int b = calculator.abs.apply(-2);
+
+        calculator.println.accept(b);
+    }
+}
