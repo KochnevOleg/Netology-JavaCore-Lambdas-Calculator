7f93fdbb07ef74bedae6ff649f0e4390c0fbcd6b
KochnevOleg <95541637+KochnevOleg@users.noreply.github.com>
2022-06-03 17:03:18 +0300

    Create package MissedCalls, change Main MissedCalls to arrays & foreach loops

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
         Calculator calculator = Calculator.instance.get();
 
         int x = calculator.plus.apply(3,67);
-        int y = calculator.divide.apply(190, 54);
+        int y = calculator.divide.apply(190, 0);
         int z = calculator.pow.apply(4);
         int a = calculator.minus.apply(45,78);
         int b = calculator.abs.apply(-2);
